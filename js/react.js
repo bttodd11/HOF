ab0fe6bf8e6ccd997bee95d0d019511961f7abfc
bttodd11 <bttodd@icloud.com>
2017-08-18 17:22:50 -0500

    Updated HOF(React)

---
+++ js/react.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const Update = function(){
+return(
+<h2> Hi </h2>
+);
+}
+
+
+
+ReactDOM.render(
+  <Update />,
+  document.getElementById('JS')
+)
