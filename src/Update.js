98257aec51b229b56b202246b3f5dff82bae04f7
bttodd11 <bttodd@icloud.com>
2017-08-27 11:09:05 -0500

    Updated React

---
+++ src/Update.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Update extends Component {
+  render() {
+    return (
+      <div className="Update">
+        <h1>
+          Hello World
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Update;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+const Update = function(){
+return(
+<h2> Hi </h2>
+);
+}
+ReactDOM.render(
+  <h1> Hello World </h1>,
+  document.getElementById('root')
+);
