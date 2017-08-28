1c2737f67869cbf5c36b1f0801a3077a967aa606
bttodd11 <bttodd@icloud.com>
2017-08-27 23:08:36 -0500

    Updated React

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import Update from "./Update";
 
 
 ReactDOM.render(
   <Update />
-  <h1> Hello World </h1>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
